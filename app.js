430f6f2203f610c107159fb342eb95e0e81439db
Martin C <98830462+Devnah4@users.noreply.github.com>
2022-05-13 13:43:47 -0400

    Added base Routes and Server info

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-// inquirer goes here
+const express = require('express');
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
+
+// Start Screen
+function startScreen() {
+    inquirer.prompt({
+        type: 'list',
+        name: 'start',
+        message: "Welcome, please select from the folowing options.",
+        choices: [
+            "View all Departments",
+            "View all Roles",
+            "View all Employees",
+            "Add a Department",
+            "Add a Role",
+            "Add an Employee",
+            "Update an Employee Role"
+        ]
+    })
+}
+
+// View all the departments
+
+// View all the roles
+
+// View all the Employees
+
+// Add a Department
+
+// Add a Role
+
+// Add an employee
+
+// Update an Employee
