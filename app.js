fbebf0ea3770f096060d2d8ef3c60d3084371dcd
Martin C <98830462+Devnah4@users.noreply.github.com>
2022-05-13 14:02:00 -0400

    Added Route inits

--- app.js
+++ app.js
@@ -1,29 +1,63 @@
-const express = require('express');
-const inquirer = require('inquirer');
-const mysql = require('mysql2');
+const express = require("express");
+const inquirer = require("inquirer");
+const mysql = require("mysql2");
 
 // Start Screen
 function startScreen() {
-    inquirer.prompt({
-        type: 'list',
-        name: 'start',
-        message: "Welcome, please select from the folowing options.",
-        choices: [
-            "View all Departments",
-            "View all Roles",
-            "View all Employees",
-            "Add a Department",
-            "Add a Role",
-            "Add an Employee",
-            "Update an Employee Role"
-        ]
+  inquirer
+    .prompt({
+      type: "list",
+      name: "start",
+      message: "Welcome, please select from the folowing options.",
+      choices: [
+        "View all Departments",
+        "View all Roles",
+        "View all Employees",
+        "Add a Department",
+        "Add a Role",
+        "Add an Employee",
+        "Update an Employee Role",
+        "Exit",
+      ],
     })
+    // Starts all of the functions to view and edit data
+    .then(function ({ initial }) {
+      switch (initial) {
+        case "View all Departments":
+          viewDepartments();
+          break;
+
+        case "View all Roles":
+          viewRoles();
+          break;
+
+        case "View all Employees":
+          viewEmployee();
+          break;
+
+        case "Add a Department":
+          addDepartment();
+          break;
+
+        case "Add a Role":
+          addRole();
+          break;
+
+        case "Add an Employee":
+          addEmployee();
+          break;
+
+        // ends the program
+        case "Exit":
+          db.end();
+          console.log("Thanks for using the Employee Tracker!");
+          break;
+      }
+    });
 }
 
 // View all the departments
-function viewDeapartments() {
-    
-}
+function viewDepartments() {}
 
 // View all the roles
 
